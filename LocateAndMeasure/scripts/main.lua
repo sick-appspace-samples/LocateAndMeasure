c56a7ad8b188173a4f265fa7c9fddb47c0ed0a1d
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-09-08 12:52:44 +0200

    'Update'

--- LocateAndMeasure/scripts/main.lua
+++ LocateAndMeasure/scripts/main.lua
@@ -1,30 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  LocateAndMeasure
-                                                                                             
-  Summary:
-  Measuring part dimensions, independent on rotation and translation
-  
-  Description:
-  Teaching the shape of a ”golden part”, measuring edge-to-edge distance,
-  circle radius etc. Matching new, identical objects with full rotation
-  in the image and measuring the same dimensions.
-  
-  How to Run:
-  Starting this sample is possible either by running the app (F5) or
-  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
-  function allows debugging step-by-step after 'Engine.OnStarted' event.
-  Results can be seen in the image viewer on the DevicePage.
-  Restarting the Sample may be necessary to show images after loading the webpage.
-  To run this Sample a device with SICK Algorithm API and AppEngine >= V2.5.0 is
-  required. For example SIM4000 with latest firmware. Alternatively the Emulator
-  in AppStudio 2.3 or higher can be used.
-       
-  More Information:
-  Tutorial "Algorithms - Fitting and Measurement".
-
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 print('AppEngine Version: ' .. Engine.getVersion())
